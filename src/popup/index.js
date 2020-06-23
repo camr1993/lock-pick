41311f18b88b84ac6cb6340d76347d77915f90d1
Cameron Ratliff <cratliff@colgate.edu>
2020-06-22 20:28:54 -0400

    added submit btn

--- src/popup/index.js
+++ src/popup/index.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
 function eventListeners() {
   $('#show-rules-btn').on('click', clickShowRules)
   $('#add-rules-btn').on('click', clickAddRules)
+  $('#report').on('click', clickReport)
 }
 
 function clickShowRules() {
@@ -17,25 +18,77 @@ function clickShowRules() {
 }
 
 function clickAddRules() {
-  addTextarea()
-  $('#add-rules-btn').attr('disabled', 'true')
+  const textarea = checkForTextarea()
+  textarea.attr('placeholder', 'Add password requirements...')
+  checkForSubmitBtn()
+}
+
+function clickReport() {
+  const textarea = checkForTextarea()
+  textarea.attr('placeholder', 'Report issue...')
+  checkForSubmitBtn()
+}
+
+function checkForTextarea() {
+  let textarea
+  // if textarea is already there, just select it and change the placeholder
+  // else create a textarea
+  if ($('textarea').length) {
+    textarea = $('textarea')
+  } else {
+    textarea = addTextarea()
+  }
+  return textarea
+}
+
+function checkForSubmitBtn() {
+  let submitBtn
+  if ($('#submit-btn').length) {
+    submitBtn = $('#submit-btn')
+  } else {
+    submitBtn = addSubmitBtn()
+  }
+  return submitBtn
 }
 
 function addTextarea() {
-  $('#main').height('320px')
+  $('#main').height('350px')
   $('<textarea/>').insertBefore('#report')
-  $('textarea')
+  return $('textarea').css({
+    resize: 'none',
+    height: '100px',
+    marginLeft: '15px',
+    marginRight: '15px',
+    fontFamily: 'system-ui, sans-serif',
+    backgroundColor: 'rgb(240, 240, 240)',
+    border: 0,
+    borderRadius: '2px',
+    padding: '5px',
+  })
+}
+
+function addSubmitBtn() {
+  $('<button id="submit-btn">Submit</button>')
+    .insertBefore('#report')
     .css({
-      resize: 'none',
-      height: '100px',
       marginLeft: '15px',
       marginRight: '15px',
       marginTop: '10px',
-      fontFamily: 'system-ui, sans-serif',
-      backgroundColor: 'rgb(240, 240, 240)',
-      border: 0,
-      borderRadius: '2px',
-      padding: '5px',
+      marginBottom: '10px',
+      height: '30px',
+      border: '0',
+      color: 'white',
+      backgroundColor: '#E6AB00',
+      borderRadius: '3px',
+      boxShadow: '0px 0.7px 2px black',
+      cursor: 'pointer',
     })
-    .attr('placeholder', 'Add password requirements...')
+    .hover(
+      function () {
+        $(this).css({ backgroundColor: '#c59503' })
+      },
+      function () {
+        $(this).css({ backgroundColor: '#E6AB00' })
+      }
+    )
 }
